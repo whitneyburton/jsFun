1d055e24464457d349dd8f6559db5b5466ccfd16
Brittany Storoz <brittanystoroz@gmail.com>
2018-10-04 14:06:52 -0600

    Add scope tests

--- test/scope-test.js
+++ test/scope-test.js
@@ -11,4 +11,105 @@ describe('SCOPE', () => {
       { 'D': 'Paul' }
     ]);
   });
+
+  it('exerciseB', () => {
+    const result = scope.exerciseB();
+    expect(result).to.deep.equal([
+      { 'A': 75 },
+      { 'B': 64 },
+      { 'C': 64 },
+      { 'D': 30 }
+    ]);
+  });
+
+  it('exerciseC', () => {
+    const result = scope.exerciseC();
+    expect(result).to.deep.equal([
+      { 'A': 'Yo' },
+      { 'B': 'Hey' },
+      { 'C': 'Hey' },
+      { 'D': 'Hello' }
+    ]);
+  });
+
+  it('exerciseD', () => {
+    const result = scope.exerciseD();
+    expect(result).to.deep.equal([
+      { 'A': 'hi' },
+      { 'B': 'welcome' },
+      { 'C': 'welcome' },
+      { 'D': 'howdy' }
+    ]);
+  });
+
+  it('exerciseE', () => {
+    const result = scope.exerciseE();
+    expect(result).to.deep.equal([
+      { 'C': 'Brittany' },
+      { 'A': 'Nathaniel' },
+      { 'B': 'Nathaniel' },
+      { 'D': 'Brittany' }
+    ]);
+  });
+
+  it('exerciseF', () => {
+    const result = scope.exerciseF();
+    expect(result).to.deep.equal([
+      { 'A': 'Spot' },
+      { 'B': 'Spot' },
+      { 'C': 'Biscuit' },
+      { 'D': 'Biscuit' },
+      { 'E': 'Biscuit' }
+    ]);
+  });
+
+  it('exerciseG', () => {
+    const result = scope.exerciseG();
+    expect(result).to.deep.equal([
+      { 'A': 'reference error' },
+      { 'B': 'mango' },
+      { 'C': 'mango' },
+      { 'D': 'apple' }
+    ]);
+  });
+
+  it('exerciseH', () => {
+    const result = scope.exerciseH();
+    expect(result).to.deep.equal([]);
+  });
+
+  it('exerciseI', () => {
+    const result = scope.exerciseI();
+    expect(result).to.deep.equal([]);
+  });
+
+  it('exerciseJ', () => {
+    const result = scope.exerciseJ();
+    expect(result).to.deep.equal([]);
+  });
+
+  it('exerciseK', () => {
+    const result = scope.exerciseK();
+    expect(result).to.deep.equal([]);
+  });
+
+  it('exerciseL', () => {
+    const result = scope.exerciseL();
+    expect(result).to.deep.equal([]);
+  });
+
+  it('exerciseM', () => {
+    const result = scope.exerciseM();
+    expect(result).to.deep.equal([]);
+  });
+
+  it('exerciseN', () => {
+    const result = scope.exerciseN();
+    expect(result).to.deep.equal([]);
+  });
+
+  it('exerciseO', () => {
+    const result = scope.exerciseO();
+    expect(result).to.deep.equal([]);
+  });
 });
