0648147834d50aba54ba76f7b38491cce2067b89
Brittany Storoz <brittanystoroz@gmail.com>
2018-10-04 13:30:24 -0600

    Add context tests

--- test/context-test.js
+++ test/context-test.js
@@ -6,4 +6,49 @@ describe('CONTEXT', () => {
     const result = context.exerciseA();
     expect(result).to.equal('instance of ship');
   });
+
+  it('exerciseB', () => {
+    const result = context.exerciseA();
+    expect(result).to.equal('window');
+  });
+
+  it('exerciseC', () => {
+    const result = context.exerciseA();
+    expect(result).to.equal('el');
+  });
+
+  it('exerciseD', () => {
+    const result = context.exerciseA();
+    expect(result).to.equal('window');
+  });
+
+  it('exerciseE', () => {
+    const result = context.exerciseA();
+    expect(result).to.equal('window');
+  });
+
+  it('exerciseF', () => {
+    const result = context.exerciseA();
+    expect(result).to.equal('instance of hero');
+  });
+
+  it('exerciseG', () => {
+    const result = context.exerciseA();
+    expect(result).to.equal('window');
+  });
+
+  it('exerciseH', () => {
+    const result = context.exerciseA();
+    expect(result).to.equal('obj');
+  });
+
+  it('exerciseI', () => {
+    const result = context.exerciseA();
+    expect(result).to.equal('poets');
+  });
+
+  it('exerciseJ', () => {
+    const result = context.exerciseA();
+    expect(result).to.equal('#btn');
+  });
 });
