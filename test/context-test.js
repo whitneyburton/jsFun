b62f326f219e99668475ee17f59bb5e8addbb416
Brittany Storoz <brittanystoroz@gmail.com>
2018-10-04 13:32:48 -0600

    Fix tests

--- test/context-test.js
+++ test/context-test.js
@@ -8,47 +8,47 @@ describe('CONTEXT', () => {
   });
 
   it('exerciseB', () => {
-    const result = context.exerciseA();
+    const result = context.exerciseB();
     expect(result).to.equal('window');
   });
 
   it('exerciseC', () => {
-    const result = context.exerciseA();
+    const result = context.exerciseC();
     expect(result).to.equal('el');
   });
 
   it('exerciseD', () => {
-    const result = context.exerciseA();
+    const result = context.exerciseD();
     expect(result).to.equal('window');
   });
 
   it('exerciseE', () => {
-    const result = context.exerciseA();
+    const result = context.exerciseE();
     expect(result).to.equal('window');
   });
 
   it('exerciseF', () => {
-    const result = context.exerciseA();
-    expect(result).to.equal('instance of hero');
+    const result = context.exerciseF();
+    expect(result).to.equal('instance of hero');  
   });
 
   it('exerciseG', () => {
-    const result = context.exerciseA();
+    const result = context.exerciseG();
     expect(result).to.equal('window');
   });
 
   it('exerciseH', () => {
-    const result = context.exerciseA();
+    const result = context.exerciseH();
     expect(result).to.equal('obj');
   });
 
   it('exerciseI', () => {
-    const result = context.exerciseA();
+    const result = context.exerciseI();
     expect(result).to.equal('poets');
   });
 
   it('exerciseJ', () => {
-    const result = context.exerciseA();
+    const result = context.exerciseJ();
     expect(result).to.equal('#btn');
   });
 });
